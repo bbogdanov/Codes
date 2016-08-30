b14cf5164a4f2df1873acf74cdffda18819e2ea3
bbogdanov <bogdanmbogdanov@gmail.com>
2016-08-30 08:49:56 +0300

    BinaryToHex and InvertBinary

---
+++ BinaryToHex/BinaryToHex.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace BinaryToHex
+{
+  class BinaryToHex
+  {
+    public static void Main()
+    {
+      string binaryNumber  = Console.ReadLine();
+      string hexNumber     = Convert.ToInt32(binaryNumber, 2).ToString("X");
+      int    decimalNumber = Convert.ToInt32(binaryNumber, 2);
+
+      Console.WriteLine("Your Decimal number is: {0}", decimalNumber);
+      Console.WriteLine("Your Hex     number is: {0}", hexNumber    );
+
+      Console.ReadLine();
+    }
+  }
+}
---
+++ InvertBinaryNumber/InvertBinary.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace InvertBinaryNumber
+{
+  class InvertBinaryNumber
+  {
+    static void Main()
+    {
+      Console.Write("Add a number: ");
+
+      int number = int.Parse(Console.ReadLine());
+      string binaryNumber = Convert.ToString(Convert.ToInt32(number), 2);
+      char[] binaryNumbers = binaryNumber.ToCharArray();
+
+      Console.WriteLine("Binary number of {0} is {1}", number, binaryNumber);
+
+      for (var i = 0; i < binaryNumbers.Length; i++) {
+        if(binaryNumbers[i] == '0') binaryNumbers[i] = '1';
+        else binaryNumbers[i] = '0';
+      }
+
+      string reverseNumber = new String(binaryNumbers);
+
+      Console.WriteLine("Inverted Binary number of {0} is {1}, and it is equals to {2}", number, reverseNumber, Convert.ToInt32(reverseNumber, 2));
+
+      Console.ReadLine();
+    }
+  }
+}
